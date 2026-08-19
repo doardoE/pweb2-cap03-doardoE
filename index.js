f327e8dcbb6e2957b56cb28360addab0fdedd274
Eduardo Pereira <doardoperera@gmail.com>
2026-08-19 19:11:42 -0300

    feat(rota-07): responde PATCH /config confirmando atualização

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ const server = http.createServer(async (req, res) => {
         return;
     }
 
+    // 7
+    if (req.method === "PATCH" && req.url === "/config") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("Configuração atualizada");
+        return;
+    }
+
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("Não Encontrado");
 });
